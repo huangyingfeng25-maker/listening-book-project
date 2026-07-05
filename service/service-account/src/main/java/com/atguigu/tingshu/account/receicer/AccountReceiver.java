3f0150a04d7aacf4e7c101fcc88d7de840ff6c8c
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-07-05 17:11:01 +0800

    创建mq接收端

--- service/service-account/src/main/java/com/atguigu/tingshu/account/receicer/AccountReceiver.java
+++ service/service-account/src/main/java/com/atguigu/tingshu/account/receicer/AccountReceiver.java
@@ -1,4 +1,36 @@
 package com.atguigu.tingshu.account.receicer;
 
+import com.atguigu.tingshu.account.service.UserAccountService;
+import com.atguigu.tingshu.common.rabbit.constant.MqConst;
+import com.atguigu.tingshu.model.account.UserAccount;
+import com.rabbitmq.client.Channel;
+import lombok.SneakyThrows;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class AccountReceiver {
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @SneakyThrows
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(value = MqConst.EXCHANGE_USER, durable = "true"),
+            value = @Queue(value = MqConst.QUEUE_USER_REGISTER, durable = "true"),
+            key = {MqConst.ROUTING_USER_REGISTER}
+    ))
+    public void receive(Long userId, Message message, Channel channel){
+        if (userId != null){
+            userAccountService.initUserAccount(userId);
+        }
+        //手动确认
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+    }
 }
